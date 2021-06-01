bc708295b1e17a69e220a8c79dc84b32bad6de4e
markcompsci <>
2021-06-01 13:39:30 +0000

    did challenges

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const btn = document.getElementById("pressBtn")
 btn.addEventListener("click", () => {
   alert("Thanks for pressing me")
 })
+
+//Extra
+const colorChangingBtn = document.getElementById("colorChangingBtn")
+const paraToChange = document.getElementById("paraChange")
+
+colorChangingBtn.addEventListener("click", () => {
+  paraToChange.style = "color: red"
+})
