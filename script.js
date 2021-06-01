8363aca2b4df50c8c1e6ee7d03d389e74bbc3f35
markcompsci <>
2021-06-01 14:23:59 +0000

    fixed linter errors

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const btn = document.getElementById("press-btn")
+const btn = document.getElementById('press-btn')
 
-btn.addEventListener("click", () => {
-  alert("Thanks for pressing me")
+btn.addEventListener('click', () => {
+  alert('Thanks for pressing me')
 })
 
 //Extra
-const colorChangingBtn = document.getElementById("color-changing-btn")
-const paraToChange = document.getElementById("para-change")
 
-colorChangingBtn.addEventListener("click", () => {
-  paraToChange.style = "color: red"
+const colorChangingBtn = document.getElementById('color-changing-btn')
+const paraToChange = document.getElementById('para-change')
+
+colorChangingBtn.addEventListener('click', () => {
+  paraToChange.style = 'color: red'
 })
